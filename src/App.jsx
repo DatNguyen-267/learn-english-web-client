041458c681e25a2f80ff2ea2c5dd2082a20b4e5a
DatN <71223440+DatNguyen-267@users.noreply.github.com>
2022-06-07 15:44:13 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-
-import { BrowserRouter, Route, Routes, Navigate, useLocation, Router, Outlet } from "react-router-dom";
-import HomePage from './pages/HomePage/HomePage'
-import CoursePage from './pages/CoursePage/CoursePage'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+  Router,
+  Outlet,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import CoursePage from "./pages/CoursePage/CoursePage";
 import Header from "./components/Header/Header";
 import NotFoundFage from "./pages/NotFoundPage/NotFoundPage";
 import { Loading_1 } from "./components/Loading/Loading_1";
-import * as courseAction from './redux/actions/index'
+import * as courseAction from "./redux/actions/index";
 import PracticeListenPage from "./pages/PracticeListenPage/PracticeListenPage";
 
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import * as actions from "./redux/actions/index";
 import axios from "axios";
-import {SERVER_URL} from './constants/index'
+import { SERVER_URL } from "./constants/index";
 import { fetchUser } from "./api";
 import { UserPage } from "./pages/UserPage/UserPage";
-import GrammarPage from './pages/CoursePage/GrammarPage'  
+import GrammarPage from "./pages/CoursePage/GrammarPage";
 import { VocaPage } from "./pages/VocaPage/VocaPage";
 
 import ListenCoursePage from "./pages/ListenCoursePage/ListenCoursePage";
@@ -27,69 +34,82 @@ import { DoTestPage } from "./pages/TestPage/DoTestPage";
 
 axios.defaults.withCredentials = true;
 function App() {
-  const loading = useSelector(state => state.loading)
-  const dispatch = useDispatch()
-  const token = useSelector(state => state.token)
-  const auth = useSelector(state => state.auth)
-  const popup = useSelector(state => state.popup)
-  
+  const loading = useSelector((state) => state.loading);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const auth = useSelector((state) => state.auth);
+  const popup = useSelector((state) => state.popup);
 
-  
-  useEffect(()=> {
-    const firstLogin = localStorage.getItem('firstLogin')
+  useEffect(() => {
+    const firstLogin = localStorage.getItem("firstLogin");
     if (firstLogin) {
-      const getToken = async() => {
-        const res = await axios.get(`http://localhost:5000/user/refresh_token`, {
-          withCredentials: true,
-        })
-        console.log("Lấy token thành công")
-        dispatch(actions.getToken(res.data.access_token))
-        dispatch(actions.login_success())
-      }
-      getToken()
+      const getToken = async () => {
+        const res = await axios.get(
+          `http://localhost:5000/user/refresh_token`,
+          {
+            withCredentials: true,
+          }
+        );
+        console.log("Lấy token thành công");
+        dispatch(actions.getToken(res.data.access_token));
+        dispatch(actions.login_success());
+      };
+      getToken();
     }
     console.log("bắt unloading");
-    dispatch(actions.unLoadingRequest())
-  }, [])
-  useEffect(()=> {
-    if(token) {
+    dispatch(actions.unLoadingRequest());
+  }, []);
+  useEffect(() => {
+    if (token) {
       const getUser = () => {
         // console.log(token)
-        return fetchUser(token).then((res)=> {
-          dispatch(actions.get_user(res.data))
+        return fetchUser(token).then((res) => {
+          dispatch(actions.get_user(res.data));
           // console.log(res)
-        })
-      }
+        });
+      };
       getUser();
     }
-  }, [auth.isLogged])
+  }, [auth.isLogged]);
   return (
-    <div >
-        { popup.login &&<PopUpLogin></PopUpLogin>}
-        {loading.isLoading ? (<Loading_1/>) : ""}
-        <Header></Header>
-        <div className="body">
-          {/* Nội dung trang */}
-          <div className="main-content">
-            <Outlet></Outlet>
-            <Routes >
-              {/* <Route path="*" element={<Navigate to="/home" />}></Route> */}
-              {/* Navigate là chuyển hướng về trang khác */}
-              <Route path="home" element={<HomePage />}></Route>
-              <Route path="/user-infor" element={<UserPage />}></Route>
-              <Route path="course/grammar" element={<GrammarPage></GrammarPage>}></Route>
-              <Route path="course/voca/:id" element={<VocaPage />}></Route>
-              <Route path="course/voca/list-word" element={<ListWordPage />}></Route>
-              <Route path="course" element={<CoursePage />}></Route>
-              <Route path="practice-listen" element={<PracticeListenPage/>}></Route>
-              <Route path="practice-listen/course/:id" element={<ListenCoursePage/>}></Route>
-              <Route path="test" element={<TestPage />}></Route>
-              <Route path="doing-test" element={<DoTestPage />}></Route>
-              
-              <Route path="*" element={<NotFoundFage />}></Route>
-            </Routes>
-          </div>
+    <div>
+      {popup.login && <PopUpLogin></PopUpLogin>}
+      {loading.isLoading ? <Loading_1 /> : ""}
+      <Header></Header>
+      <div className="body">
+        {/* Nội dung trang */}
+        <div className="main-content">
+          <Outlet></Outlet>
+          <Routes>
+            <Route path="/" element={<Navigate to="/course" />}></Route>
+            {/* Navigate là chuyển hướng về trang khác */}
+            <Route path="home" element={<HomePage />}></Route>
+            <Route path="/user-infor" element={<UserPage />}></Route>
+            <Route
+              path="course/grammar"
+              element={<GrammarPage></GrammarPage>}
+            ></Route>
+            <Route path="course/voca/:id" element={<VocaPage />}></Route>
+            <Route
+              path="course/voca/list-word"
+              element={<ListWordPage />}
+            ></Route>
+            <Route path="course" element={<CoursePage />}></Route>
+            <Route
+              path="practice-listen"
+              element={<PracticeListenPage />}
+            ></Route>
+            <Route
+              path="practice-listen/course/:id"
+              element={<ListenCoursePage />}
+            ></Route>
+            <Route path="test" element={<TestPage />}></Route>
+            <Route path="doing-test" element={<DoTestPage />}></Route>
+            {/* <Route path="/" element={<CoursePage />}></Route> */}
+            <Route path="*" element={<NotFoundFage />}></Route>
+          </Routes>
         </div>
+      </div>
     </div>
   );
 }
--- src/components/VocaLearning/VocaInfo.jsx
+++ src/components/VocaLearning/VocaInfo.jsx
@@ -1,94 +1,111 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { ErrMsg } from '../../util/Notify/Notification'
-import speak from '../../util/speech'
-import './VocaInfo.scss'
-import axios from 'axios'
-import {SERVER_URL} from './../../constants/index'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { ErrMsg } from "../../util/Notify/Notification";
+import speak from "../../util/speech";
+import "./VocaInfo.scss";
+import axios from "axios";
+import { SERVER_URL } from "./../../constants/index";
 export const VocaInfo = ({ data, err }) => {
-  const { speak, speakSlow } = useSelector(state => state.speak)
-  const token = useSelector(state => state.token)
-  const [isSave, setIsSave] = useState(data.isSave)
+  const { speak, speakSlow } = useSelector((state) => state.speak);
+  const token = useSelector((state) => state.token);
+  const [isSave, setIsSave] = useState(data.isSave);
 
   const handleSpeech = () => {
-    speak.text = data.english;
-    speechSynthesis.speak(speak)
-  }
+    // speak.text = data.english;
+    // speechSynthesis.speak(speak);
+    let audio = new Audio(
+      `https://600tuvungtoeic.com//audio/${data.english}.mp3`
+    );
+    audio.play();
+  };
   const handleSpeechEx = () => {
     speakSlow.text = data.example.sentense;
-    speechSynthesis.speak(speakSlow)
-  }
+    speechSynthesis.speak(speakSlow);
+  };
   const handleSave = () => {
     if (isSave === false) {
       try {
         const addWordToStore = async () => {
-          let res = await axios.post(`${SERVER_URL}/courses/voca/addWordToStore`,{
-            idWord: data._id
-          },{
-            headers: {Authorization: token}
-          })
-          if (res.data === "success" ) {
-            data.isSave = true
-            setIsSave(data.isSave)
+          let res = await axios.post(
+            `${SERVER_URL}/courses/voca/addWordToStore`,
+            {
+              idWord: data._id,
+            },
+            {
+              headers: { Authorization: token },
+            }
+          );
+          if (res.data === "success") {
+            data.isSave = true;
+            setIsSave(data.isSave);
           }
           console.log(res);
-        }
-        addWordToStore()
-      } catch (error) {
-  
-      }
-    }
-    else {
+        };
+        addWordToStore();
+      } catch (error) {}
+    } else {
       try {
         const removeWordToStore = async () => {
-          let res = await axios.post(`${SERVER_URL}/courses/voca/removeWordOfStore`,{
-            idWord: data._id
-          },{
-            headers: {Authorization: token}
-          })
-          if (res.data === "success" ) {
-            data.isSave = false
-            setIsSave(data.isSave)
+          let res = await axios.post(
+            `${SERVER_URL}/courses/voca/removeWordOfStore`,
+            {
+              idWord: data._id,
+            },
+            {
+              headers: { Authorization: token },
+            }
+          );
+          if (res.data === "success") {
+            data.isSave = false;
+            setIsSave(data.isSave);
           }
           console.log(res);
-        }
-        removeWordToStore()
-      } catch (error) {
-  
-      }
+        };
+        removeWordToStore();
+      } catch (error) {}
     }
-    
-  }
+  };
   return (
     <div className="learn__body-info">
-      {err === true && <div className='learn__body-info-errMsg'>
-        Bạn trả lời sai từ này
-      </div>}
+      {err === true && (
+        <div className="learn__body-info-errMsg">Bạn trả lời sai từ này</div>
+      )}
       <div className="learn__body-item">
-        <div className="learn__body-item-content main">{data ? data.english : ""}
-          <i className="learn__body-item-icon fas fa-volume-up" onClick={handleSpeech}></i>
-          <i className={`learn__body-item-icon fa fa-heart ${isSave ? "active" : ""}`} onClick={handleSave}></i>
+        <div className="learn__body-item-content main">
+          {data ? data.english : ""}
+          <i
+            className="learn__body-item-icon fas fa-volume-up"
+            onClick={handleSpeech}
+          ></i>
+          <i
+            className={`learn__body-item-icon fa fa-heart ${
+              isSave ? "active" : ""
+            }`}
+            onClick={handleSave}
+          ></i>
         </div>
-        <div className="sub-info">/bɔːrd/</div>
+        <div className="sub-info">{data.phonetic}</div>
       </div>
       <div className="learn__body-item">
         <div className="learn__body-item-title">Tiếng việt</div>
         <div className="learn__body-item-content">
-          ({data ? data.meanings[0].partOfSpeech : ""}) {data ? data.meanings[0].vietnamese : ""}
+          ({data ? data.meanings[0].partOfSpeech : ""}){" "}
+          {data ? data.meanings[0].vietnamese : ""}
         </div>
       </div>
       <div className="learn__body-item">
         <div className="learn__body-item-title">Đồng nghĩa</div>
-        <div className="learn__body-item-content">{data ? data.meanings[0].synonyms : ""}</div>
+        <div className="learn__body-item-content">
+          {data ? data.meanings[0].synonyms : ""}
+        </div>
       </div>
       <div className="learn__body-item">
         <div className="learn__body-item-title">Ví dụ</div>
         <div className="learn__body-item-example">
           <i className="fas fa-volume-up" onClick={handleSpeechEx}></i>
-          <span>{data ? data.example.sentense : ""}
-          </span>
+          <span>{data ? data.example.sentense : ""}</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
