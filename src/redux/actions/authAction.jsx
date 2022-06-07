2d84cb29becd734dd7b9c6aecb7a4455af5554fd
NguyenHoangNam-19520171 <71280485+NguyenHoangNam-19520171@users.noreply.github.com>
2022-06-07 23:39:16 +0700

    commit

--- src/redux/actions/authAction.jsx
+++ src/redux/actions/authAction.jsx
@@ -1,7 +1,8 @@
-import { FETCH_USER, GET_TOKEN, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT } from "../../constants/actionTypes";
+import { FETCH_USER, GET_TOKEN, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, RESET_TOKEN } from "../../constants/actionTypes";
 
 export const login_request = (payload) => {return {type: LOGIN_REQUEST, payload}}
 export const login_success = (payload) => {return {type: LOGIN_SUCCESS,payload}}
 export const login_failure = (payload) => {return {type: LOGIN_FAILURE,payload}}
 export const logout = (payload) => {return {type: LOGOUT,payload}}
 export const getToken = (payload) => {return {type: GET_TOKEN,payload}}
+export const resetToken = (payload) => {return {type: RESET_TOKEN,payload}}
