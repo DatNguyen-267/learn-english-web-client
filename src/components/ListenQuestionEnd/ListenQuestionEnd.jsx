1024a40c7677a78dda5b305558a4f9bb0a44eedf
NguyenHoangNam-19520171 <71280485+NguyenHoangNam-19520171@users.noreply.github.com>
2022-08-29 16:30:01 +0700

    commit

--- src/components/ListenQuestionEnd/ListenQuestionEnd.jsx
+++ src/components/ListenQuestionEnd/ListenQuestionEnd.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ListenMinQuestion from '../ListenMinQuestion/ListenMinQuestion';
 import "./ListenQuestionEnd.scss"
-import {  useNavigate, useLocation } from 'react-router-dom';
-function ListenQuestionEnd({list_question, question_true, ls,  part_id}) {
-    let ls_course_id =""
-    let total_question = 0
+import { useNavigate, useLocation } from 'react-router-dom';
+function ListenQuestionEnd({ list_question, question_true,setQuestionTrue, ls, part_id, total_question,list_user_answer}) {
+    let ls_course_id = ""
+    let index_question = 0
     const navigate = useNavigate()
     const location = useLocation()
-    const handleBack = () =>{
-        try{
+    
+    useEffect(() => {
+        handleLoad()
+    }, [])
+    const handleBack = () => {
+        try {
             navigate(`/practice-listen/part/${part_id}`)
             window.location.reload()
         }
-        catch (error){
+        catch (error) {
 
         }
-       
+
     }
-    const handleNext = () =>{
+    const handleNext = () => {
         try {
-            if(ls){
+            if (ls) {
                 ls.forEach(element => {
-                    element.list_course.forEach(course=>{
-                        course.list_part.forEach(part=>{
-                            if(part == part_id){
+                    element.list_course.forEach(course => {
+                        course.list_part.forEach(part => {
+                            if (part == part_id) {
                                 ls_course_id = course._id
                             }
                         })
@@ -31,33 +35,147 @@ function ListenQuestionEnd({list_question, question_true, ls,  part_id}) {
                 })
             }
             navigate(`/practice-listen/course/${ls_course_id}`)
-        } 
+        }
         catch (error) {
-        
+
         }
     }
-
-    if(list_question){
-        list_question.map((item, index)=>{
-            total_question = total_question+item.list_min_question.length;
+    const handleLoad = () => {
+        var question_true_total = 0
+        var list_min_question = []
+        list_question.forEach((question) => {
+            if (question.transcript) {
+                document.getElementById(`transcript${question._id}`).style.display = "block"
+            }
+            question.list_min_question.forEach((min_quest) => {
+                list_min_question.push(min_quest)
+            })
         })
+        var i = 0
+        const min_questions = document.querySelectorAll('.min-question')
+        console.log("min question: ", min_questions)
+        console.log("list user answer: ", list_user_answer)
+        // kiem tra dung sai
+        min_questions.forEach(min_quest => {
+            const min_questiontitle = min_quest.querySelector('.min-question__title')
+            min_questiontitle.querySelector('span').classList.remove("hide-content")
+            var answers = min_quest.querySelectorAll('.answers-item')
+            var min_question = list_min_question[i]
+            answers.forEach(item => {
+                var input = item.querySelector('input')
+                var user_answer = list_user_answer[i]
+                item.lastChild.classList.remove("hide-content")
+                // data: chua id cua cau tra loi
+                const data = item.getAttribute('data-value')
+                if (user_answer != data && data != min_question.true_answer) {
+                    item.classList.add("dont-choose-answer")
+                    answers.forEach(item2 => {
+                        const data2 = item2.getAttribute('data-value')
+                        if (data2 == min_question.true_answer) {
+                            item2.classList.remove("dont-choose-answer")
+                            item2.classList.add("correct-answer")
+                        }
+                    })
+                }
+                if (user_answer == data && data == min_question.true_answer) {
+                    item.classList.add("correct-answer")
+                    input.checked = true
+                    question_true_total++
+                    console.log("end page:", question_true_total)
+                }
+                if (user_answer == data && data != min_question.true_answer) {
+                    input.checked = true
+                    item.classList.add("fail-answer")
+                    answers.forEach(item2 => {
+                        const data2 = item2.getAttribute('data-value')
+
+                        if (data2 == min_question.true_answer) {
+                            item2.classList.remove("dont-choose-answer")
+                            item2.classList.add("correct-answer")
+                        }
+                    })
+
+                }
+                item.querySelector('input').disabled = true
+            })
+            i++
+            console.log(question_true_total)
+        })
+        setQuestionTrue(question_true_total)
     }
+    
     return (
-        
+
         <div className='lsquestion-end'>
             <div className='lsquestion-end-main-title'>
-              Chúc mừng bạn đã hoàn thành
+                Chúc mừng bạn đã hoàn thành
             </div>
             <div className='lsquestion-end-sub-title'>
                 <span className='sub-title-total-number'>Tổng số câu: {total_question}</span><br></br>
                 <span className='sub-title-correct-number'>Số câu đúng: {question_true}</span>
             </div>
             <div className='lsquestion-end-option'>
-                <button  onClick={handleBack} className='lsquestion-end-btn-back'>Thử lại</button >
+                <button onClick={handleBack} className='lsquestion-end-btn-back'>Thử lại</button >
                 <button onClick={handleNext} className='lsquestion-end-btn-next'>Tiếp tục</button >
             </div>
+            <div class="lsquestion-end__questions grid wide" id="slider" >
+                <div className='lsquestion-end__questions__title'>
+                    <span>Bài làm</span>
+                </div>
+                {
+
+                    list_question.map((item, index_qs) => {
+                        const question = item
+                        const list_min_question = item.list_min_question;
+                        index_question = list_min_question.length + index_question
+                        return (
+                            <div className="col l-12 m-12 c-12">
+                                <div class="lsquestion-end-item">
+                                    {
+                                        question.img ? <div class="lsquestion-end-item__header"> <img src={require(`../../assets/img/ls-question/${question.img}`)} alt="image" /></div> : ""
+                                    }
+                                    <div className='lsquestion-end-item__audio'>
+                                        <audio controls="block" src={(item && item.sound) ? require(`../../assets/sound/${item.sound}`) : ""}></audio>
+                                    </div>
+                                    <div class="lsquestion-end-item__content">
+                                        {
+
+                                            list_min_question.map((item, index) => {
+
+                                                return (
+                                                    <ListenMinQuestion min_question={item}
+                                                        index={list_min_question.length === 1 ? 0 : index}
+                                                        index_qs={index_question - list_min_question.length}
+                                                        setQuestionTrue={setQuestionTrue}
+                                                        question_true={question_true}
+                                                        // toggleTranscript={toggleTranscript}
+                                                        length={list_min_question.length}
+
+                                                    >
+                                                    </ListenMinQuestion>
+
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    {
+                                        question.transcript ?
+                                            <div class="lsquestion-end-item__transcript" id={`transcript${question._id}`}>
+                                                <div className='transcript-title'>Transcript:</div>
+                                                <div className='transcript-content' dangerouslySetInnerHTML={{ __html: question.transcript }}>
+                                                </div>
+                                            </div>
+                                            : ""
+                                    }
+                                </div>
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
         </div>
-       
+
     );
 }
 
--- src/pages/ListenCoursePage/ListenCoursePage.jsx
+++ src/pages/ListenCoursePage/ListenCoursePage.jsx
@@ -1,54 +1,90 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useLocation, useParams } from 'react-router-dom';
 import LsPart from '../../components/LsPart/LsPart';
+import { useNavigate } from "react-router-dom";
 import "./ListenCoursePage.scss"
 import * as actions from './../../redux/actions/index'
 function ListenCoursePage() {
     const param = useParams()
     const id = param.id
     const dispatch = useDispatch()
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(actions.getListenCourseRequest(id))
-    },[dispatch])
-   
-   
-    const data = useSelector(state => state.listenCourse.data )
+    }, [dispatch])
+
+
+    const data = useSelector(state => state.listenCourse.data)
+    const [showPopUp, setShowPopUp] = useState(false);
+    const [targetPart, setTargetPart] = useState(null);
+    const navigator = useNavigate();
+
     let lscourse = {}
     for (const item in data) {
-        if(data[item]._id === id){
+        if (data[item]._id === id) {
             lscourse = data[item]
         }
     }
     let list_part = []
-    if(lscourse && lscourse.list_part){
+    if (lscourse && lscourse.list_part) {
         list_part = lscourse.list_part
     }
+
+    const handleClick = (item) => {
+        setTargetPart(item)
+        setShowPopUp(true);
+    };
+    const handleYes = () => {
+        navigator(`/practice-listen/part/${targetPart._id}`);
+    };
+    const handleNo = () => {
+        setShowPopUp(false);
+    };
+
     return (
-        <div className="app-course-listen grid wide">
-            <div className="listen-part">
-                <div className="listen-part__header">
-                    <div className="listen-part__header-content">
-                        <div className="listen-part__header-content__title">
-                            <h1>
-                               {lscourse? lscourse.name:''}
-                            </h1>
-                            <p>
-                                Danh sách các part
-                            </p>
+        <div className="app-course-listen">
+            {showPopUp && (
+                <div className="pop-up">
+                    <div className="modal">
+                        <div className="modal-overlay" onClick={handleNo}></div>
+                        <div className="lscourse-page__modal-body modal-body">
+                            <div className="lscourse-page__modal-title">
+                                Bạn đã sẵn sàng bắt đầu làm bài
+                            </div>
+                            <div className="lscourse-page__modal-sub-title">{targetPart.name}</div>
+                            <div className="lscourse-page__modal-option">
+                                <button onClick={handleNo}>No</button>
+                                <button onClick={handleYes}>Yes</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="listen-part__content">
-                    <div className="parts row">
-                       {
-                           list_part.map((item)=>{
-                                return(
-                                    <LsPart part={item}/>
-                                )
-                            }) 
-                        }
+            )}
+            <div className='app-course-listen-content grid wide'>
+                <div className="listen-part">
+                    <div className="listen-part__header">
+                        <div className="listen-part__header-content">
+                            <div className="listen-part__header-content__title">
+                                <h1>
+                                    {lscourse ? lscourse.name : ''}
+                                </h1>
+                                <p>
+                                    Danh sách các part
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="listen-part__content">
+                        <div className="parts row">
+                            {
+                                list_part.map((item) => {
+                                    return (
+                                        <LsPart part={item} handleClick={handleClick}/>
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
